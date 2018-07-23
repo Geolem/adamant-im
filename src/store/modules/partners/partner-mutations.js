ae70f50781ef7e1def2c6c232a35f4093cf9d017
adamant-al <33592982+adamant-al@users.noreply.github.com>
2018-07-23 13:55:46 +0300

    Update partner-mutations.js

--- src/store/modules/partners/partner-mutations.js
+++ src/store/modules/partners/partner-mutations.js
@@ -33,7 +33,7 @@ export default {
   address (state, payload) {
     console.log('Mutations-address:')
       console.log('state.list[payload.partner] obj BEFORE: ')
-      console.log(JSON.stringify(state.list[payload.partner])    
+      console.log(JSON.stringify(state.list[payload.partner]))    
     state.list[payload.partner] = Object.assign({ }, state[payload.partner],
       { [payload.crypto]: payload.address })
       console.log('state.list[payload.partner] obj AFTER: ')
